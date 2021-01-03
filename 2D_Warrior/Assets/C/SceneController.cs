46350a0d622cf42322d49524b9d9b9a0e427cd4a
a0930014306 <67904630+a0930014306@users.noreply.github.com>
2021-01-03 15:40:15 +0800

    碰撞器設定

--- 2D_Warrior/Assets/C/SceneController.cs
+++ 2D_Warrior/Assets/C/SceneController.cs
@@ -3,32 +3,66 @@ using UnityEngine;
 
 public class SceneController : MonoBehaviour
 {
+    [Header("音效來源")]
+    public AudioSource aud;
+    [Header("按鈕音效")]
+    public AudioClip soundtrack;
+
     //1.方法要讓按鈕呼叫必須設為公開 public
+
+    
+
+    private void DelayStartgame()
+    {
+
+        //2.必須將場景放在 File > BuildSettings ...
+        //場景管理器.載入場景(場景名稱);
+        SceneManager.LoadScene("地板牆壁");
+    }
+
+    private void DelayBackToMenu()
+    {
+        SceneManager.LoadScene("選單");
+    }
+
+    private void DelayQuitGame()
+    {
+
+        //2.必須將場景放在 File > BuildSettings ...
+        //場景管理器.載入場景(場景名稱);
+        Application.Quit();
+    }
 
     /// <summary>
     /// 開始遊戲
     /// </summary>
     public void Startgame()
     {
-        //2.必須將場景放在 File > BuildSettings ...
-        //場景管理器.載入場景(場景名稱);
-        SceneManager.LoadScene("地板牆壁");
+        //音效來源,播放一次(音效, 音量)
+        aud.PlayOneShot(soundtrack);
+
+        Invoke("DelayStartgame", 1.5f);
     }
+
 
     /// <summary>
     /// 返回選單
     /// </summary>
     public void BackToMenu()
     {
-        SceneManager.LoadScene("選單");
+        aud.PlayOneShot(soundtrack);
+        Invoke("DelayBackToMenu", 1.5f);
     }
+
+
     /// <summary>
     /// 離開遊戲
     /// </summary>
     public void QuitGame()
     {
+        aud.PlayOneShot(soundtrack);
         //應用程式.離開
-        Application.Quit();
+        Invoke("DelayQuitGame", 1.5f);
     }
 
 
