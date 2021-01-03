01d860aa17df603dd470ce62751e3206f8bfafc3
a0930014306 <67904630+a0930014306@users.noreply.github.com>
2021-01-03 13:01:31 +0800

    場景切換

---
+++ 2D_Warrior/Assets/C/SceneController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine.SceneManagement; //引用 場景管理 API
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    //1.方法要讓按鈕呼叫必須設為公開 public
+
+    /// <summary>
+    /// 開始遊戲
+    /// </summary>
+    public void Startgame()
+    {
+        //2.必須將場景放在 File > BuildSettings ...
+        //場景管理器.載入場景(場景名稱);
+        SceneManager.LoadScene("地板牆壁");
+    }
+
+    /// <summary>
+    /// 返回選單
+    /// </summary>
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("選單");
+    }
+    /// <summary>
+    /// 離開遊戲
+    /// </summary>
+    public void QuitGame()
+    {
+        //應用程式.離開
+        Application.Quit();
+    }
+
+
+
+}
