7a064cea6381f4225b6589d0b7df5404a5a8da96
a0930014306 <67904630+a0930014306@users.noreply.github.com>
2021-01-03 17:00:13 +0800

    角色移動

--- 2D_Warrior/Assets/C/player.cs
+++ 2D_Warrior/Assets/C/player.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    #region 角色欄位
     [Header("移動速度"), Range(0, 1000)]
-    public float movespeed = 10.5f;
+    public float movespeed = 15;
     [Header("跳躍高度"), Range(0, 3000)]
     public int jump = 100;
     [Header("是否在地板上")]
@@ -21,13 +22,49 @@ public class player : MonoBehaviour
     public int hp = 100;
 
     private AudioSource aud;
-    private Rigidbody2D rid;
+    private Rigidbody2D rig;
     private Animator ani;
 
+    #endregion
+
+    public float h;
+
+    private void Start()
+    {
+        //GetComponent<泛型>()
+        //泛型:泛指所有類型
+        //GetComponent<Animator>()
+        //GetComponent<AudioSource>()
+
+        //剛體欄位 = 取得原件<剛體>();
+        rig = GetComponent<Rigidbody2D>();
+
+    }
+    public void Update()
+    {
+        GetHorizontal();
+        Move();
+    }
+    /// <summary>
+    /// 取得水平軸向
+    /// </summary>
+    public void GetHorizontal()
+    {
+        //浮點數 = 輸入.取得軸向("水平")
+        h = Input.GetAxis("Horizontal");
+    }
+
+   
     private void Move()
     {
+
+        //剛體.加速度 = 二維(水平 * 速度, 剛體原本的加速度 Y);
+        rig.velocity = new Vector2(h * movespeed, rig.velocity.y);
         
+       
     }
+
+    
 
     private void Jump()
     {
