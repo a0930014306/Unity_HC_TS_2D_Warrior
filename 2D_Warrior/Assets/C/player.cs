8a169d0f351b179655b5eaf897118392dd8d786e
a0930014306 <67904630+a0930014306@users.noreply.github.com>
2021-01-10 12:09:25 +0800

    跳躍

--- 2D_Warrior/Assets/C/player.cs
+++ 2D_Warrior/Assets/C/player.cs
@@ -7,7 +7,7 @@ public class player : MonoBehaviour
     [Header("移動速度"), Range(0, 1000)]
     public float movespeed = 15;
     [Header("跳躍高度"), Range(0, 3000)]
-    public int jump = 100;
+    public int jump = 750;
     [Header("是否在地板上")]
     public bool ground = true;
     [Header("子彈")]
@@ -38,12 +38,15 @@ public class player : MonoBehaviour
 
         //剛體欄位 = 取得原件<剛體>();
         rig = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
+
 
     }
     public void Update()
     {
         GetHorizontal();
         Move();
+        Jump();
     }
     /// <summary>
     /// 取得水平軸向
@@ -60,15 +63,39 @@ public class player : MonoBehaviour
 
         //剛體.加速度 = 二維(水平 * 速度, 剛體原本的加速度 Y);
         rig.velocity = new Vector2(h * movespeed, rig.velocity.y);
-        
-       
+
+        //如果 玩家 按下 D 或 右鍵 就執行(內容)
+        if (Input.GetKeyDown(KeyCode.D) || (Input.GetKeyDown(KeyCode.RightArrow)))
+        {
+            //transform 指的是與此腳本同一層的 元件
+            //Rotation 角度在程式是 localEulerAngles
+            transform.localEulerAngles = Vector3.zero;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A) || (Input.GetKeyDown(KeyCode.LeftArrow)))
+        {
+            transform.localEulerAngles =new Vector3(0,180,0);
+
+        }
+
+        ani.SetBool("跑步開關", h != 0);
     }
 
     
 
     private void Jump()
     {
+        //如果 在地板上 且 按下空白鍵 才可以跳躍
+        if (ground && Input.GetKeyDown(KeyCode.Space))
+        {
+            //剛體.添加推力(二維向量);
+            rig.AddForce(new Vector2(0 , jump));
+            //不在地板上
+            ground = false;
+            
+        }
 
+        
     }
 
     private void Shoot()
